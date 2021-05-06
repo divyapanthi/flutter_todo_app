ddead665168640ae9e37cbf8eb3cbc490bf548b3
Divya <deevyapanthi03@gmail.com>
2021-05-06 06:35:50 +0545

    Created a createtodo screen on floating action button click and added title and select date section in createtodo screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/src/app.dart';
+import 'package:todo_app/src/Screens/homeScreen.dart';
 
 void main() {
   runApp((App()));
+}
+
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }
