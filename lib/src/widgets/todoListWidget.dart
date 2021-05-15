ab018ca7132404f428323a13a97d4ee53797507d
Divya <deevyapanthi03@gmail.com>
2021-05-15 08:26:27 +0545

    Added green checked functionality if the completion date expires.

--- lib/src/widgets/todoListWidget.dart
+++ lib/src/widgets/todoListWidget.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/src/models/todoModel.dart';
+import 'package:todo_app/src/screens/createTodo.dart';
 
 class TodoListWidget extends StatefulWidget {
 
@@ -12,6 +14,7 @@ class TodoListWidget extends StatefulWidget {
 
 class TodoListWidgetState extends State<TodoListWidget> {
 
+  bool isCompleted = false;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -23,30 +26,44 @@ class TodoListWidgetState extends State<TodoListWidget> {
   }
 
   Widget buildTodoItem(Todo todo) {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    if(todo.date!.isBefore(today)){
+        isCompleted = true;
+    }
+    else{
+        isCompleted = false;
+    }
     return Card(
       margin: EdgeInsets.all(12),
-      child: Container(
-        padding: EdgeInsets.all(12),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(todo.title!,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
-                ),
-                SizedBox(height: 8,),
-                Text(todo.description!,
-                  style: TextStyle(fontSize: 16, color: Colors.blueGrey),),
-                SizedBox(height: 6,),
+      child: InkWell(
+        splashColor: Colors.amber,
+        highlightColor: Colors.cyan,
+        onTap: (){
+
+        },
+        child: Container(
+          padding: EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              isCompleted ? buildIconButton() :
                 Text(todo.date.toString().split(' ')[0],
-                  style: TextStyle(fontSize: 14, color: Colors.black.withOpacity(0.4),)),
-              ],
-            ),
-            IconButton(
+                  style: TextStyle(fontSize: 16, color: Colors.deepPurple.withOpacity(0.8),)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(todo.title!,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54),
+                  ),
+                  SizedBox(height: 8,),
+                  Text(todo.description!,
+                    style: TextStyle(fontSize: 16, color: Colors.blueGrey),),
+                ],
+              ),
+              IconButton(
                 icon: Icon(Icons.delete),
                 iconSize: 36,
                 color: Colors.blueGrey,
@@ -54,16 +71,25 @@ class TodoListWidgetState extends State<TodoListWidget> {
                 onPressed: () {
                   showAlertDialog(todo);
                 },
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
 
   }
 
+  IconButton buildIconButton() {
+    return IconButton(
+      icon: Icon(Icons.check_circle),
+      iconSize: 32,
+      color: Colors.green,
+      onPressed: (){}
+      );
+  }
 
-   void showAlertDialog(Todo todo) {
+  void showAlertDialog(Todo todo) {
       //set up the buttons
      Widget cancelButton = TextButton(
        child:  const Text('Cancel'),
@@ -82,7 +108,7 @@ class TodoListWidgetState extends State<TodoListWidget> {
 
      //set up the Alert Dialog
      AlertDialog alert = AlertDialog(
-       title: Text("Delete ${todo.title} ?"),
+       title: Text("Delete task ${todo.title} ?"),
        content: Text("This task will be removed from your todo list."),
        actions: [
          cancelButton,
@@ -103,10 +129,6 @@ class TodoListWidgetState extends State<TodoListWidget> {
       widget.todolist.removeWhere((selectedTodo) => selectedTodo.title == title);
     });
   }
-
-
-
-  
 
 }
 
